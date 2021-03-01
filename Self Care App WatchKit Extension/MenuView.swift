28b4869ba61e8863d2b0d951418ff9932b65ab3e
Lucas de Oliveira Carvalho <lucas.carvalho@icomp.ufam.edu.br>
2021-03-01 16:45:11 -0400

    Menu adicionado ao Apple Watch

---
+++ Self Care App WatchKit Extension/MenuView.swift
@@ -0,0 +1,54 @@
+//
+//  MenuView.swift
+//  Self Care App WatchKit Extension
+//
+//  Created by Lucas Carvalho on 01/03/21.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        GeometryReader { g in
+            VStack {
+                Spacer()
+                // adicionar View com momentos como destination
+                NavigationLink(
+                    destination: ContentView(),
+                    label: {
+                        Button(image: "p1", title: "Momentos")
+                    }).buttonStyle(PlainButtonStyle())
+                Spacer()
+                // adicionar View com ciclos como destination
+                NavigationLink(
+                    destination: ContentView(),
+                    label: {
+                        Button(image: "p2", title: "Ciclos")
+                    }).buttonStyle(PlainButtonStyle())
+                Spacer()
+            }
+        }
+    }
+}
+
+struct Button : View {
+    
+    var image : String
+    var title : String
+    
+    var body : some View {
+        HStack {
+            Text(title)
+                .padding()
+            Spacer()
+        }
+        .background(Color(.gray))
+        .cornerRadius(15.0)
+    }
+}
+
+struct MenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuView()
+    }
+}
