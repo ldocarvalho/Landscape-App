6f6a2beb75fa66ce704f644a80601d4a625ac8df
vidal <victor.matheus.o.vidal@gmail.com>
2021-03-09 13:30:32 -0400

    user default

--- Self Care App/Self_Care_AppApp.swift
+++ Self Care App/Self_Care_AppApp.swift
@@ -10,8 +10,15 @@ import SwiftUI
 @main
 struct Self_Care_AppApp: App {
     let container = PersistenceController.shared.container
+    let firstUse = UserDefaults.standard.value(forKey: "isFirtUse")
     var body: some Scene {
         WindowGroup {
+            if (firstUse ?? true) as! Bool{
+                
+            }
+            else{
+                
+            }
                 ContentView().environment(\.managedObjectContext, container.viewContext)
            
         }
