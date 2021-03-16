4eebc0a37ef47a6079ec66cfbee6278a75d79805
Lucas de Oliveira Carvalho <lucas.carvalho@icomp.ufam.edu.br>
2021-03-16 15:18:50 -0400

    Blur arrumado nos alerts customizados da tela de detalhes

--- Self Care App/MomentDetailView.swift
+++ Self Care App/MomentDetailView.swift
@@ -30,9 +30,9 @@ struct MomentDetailView: View {
                                 .resizable()
 //                                .opacity((shownDeleteAlert || shownDoneAlert) ? 0.3 : 1)
                                 .blur(radius: (shownDeleteAlert || shownDoneAlert) ? 1 : 0)
-                                .frame(width: g.size.width, height: g.size.width*0.7, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                                .frame(width: g.size.width*1.01, height: g.size.width*0.7, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
                             
-                        }.offset(y: -g.frame(in: .global).minY)
+                        }.offset(y: -g.frame(in: .global).minY-10)
                         
                         
                         VStack {
@@ -128,7 +128,7 @@ struct MomentDetailView: View {
                         .clipShape(CustomCorner())
                         .offset(y: -g.frame(in: .global).minY - 50)
 //                        .opacity((shownDeleteAlert || shownDoneAlert) ? 0.3 : 1)
-                        .blur(radius: (shownDeleteAlert || shownDoneAlert) ? 1 : 0)
+                        .blur(radius: (shownDeleteAlert || shownDoneAlert) ? 2 : 0)
                 
                 
                 if shownDeleteAlert {
